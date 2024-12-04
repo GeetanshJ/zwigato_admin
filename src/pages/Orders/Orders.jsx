0cfb6dcd7024f300e4da2a12021c5ad01cd7cfa4
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-12-04 21:18:24 +0530

    created all orders route

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Orders.css'
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { assets } from '../../assets/assets';
+const Orders = ({url}) => {
 
-const Orders = () => {
+  const[orders,setOrders] = useState([]);
+
+  const fetchOrders = async () =>{
+    const response = axios.get(url+"/api/order/list");
+    if(response.data.success){
+      setOrders(response.data.data);
+    }
+
+    else{
+      toast.error("Error");
+    }
+  }
+
+  useEffect(() => {
+    fetchOrders();
+  },[])
+
   return (
-    <div>
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {
+          orders.map((order,index) => {
+            <div className="order-item" key={index}>
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                <div className="order-item-food">
+                  {
+                    order.items.map((item,index)=>{
+                      if(index == order.items.length - 1){
+                        return item.name + " x " + item.quantity;
+                      }
 
+                      else{
+                        return item.name + " x " + item.quantity + ", "
+                      }
+                    })
+                  }
+                </div>
+              </div>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
